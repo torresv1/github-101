c256451e9c72d8e2d132ceb2a244a00d29162996
torresv1 <torres_vladimir@hotmail.com>
2025-05-31 18:49:27 -0400

    added name parameter

--- second_file.py
+++ second_file.py
@@ -5,8 +5,8 @@
 def print_hello_world():
     print("Hello world")
 
-def print_my_name():
-    print("Hi! My name is Vladimir!") 
+def print_my_name(name):
+    print("Hi! My name is {name}!") 
 
 
 print_hello_world()
